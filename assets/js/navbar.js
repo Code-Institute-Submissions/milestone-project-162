da3c06963b9a2c3214530564759063506dc1a6f0
annakovesdi <annakovesdi@gmail.com>
2020-12-18 15:04:23 +0000

    started to edit cards, they flip back and forth now

--- assets/js/navbar.js
+++
@@ -1,7 +0,0 @@
-//how to copy to clipboard https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
-function copyToClipboard() {
-    let copyText = "https://annakovesdi.github.io/milestone-project-2/";
-    copyText.select();
-    document.execCommand("copy");
-    alert("Copied the text: " + copyText.value);
-}
