8415eb67e69abb28fdc36ff9228829ffdab071c8
annakovesdi <annakovesdi@gmail.com>
2021-01-20 20:19:10 +0000

    deleted all code from tutorials, working on js and css. Cards flip, match or mismatch and flip back.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,69 @@
 
+const card = document.getElementsByClassName('card');
+const cardsArr = Array.from(card);
+
+let clickedCard = false;
+let cardOne;
+let cardTwo; 
+
+function flipCard() {
+    this.classList.add('flip');
+
+    if (!clickedCard) {
+        clickedCard = true;
+        console.log("firstcard");
+        cardOne = this;
+    } else {
+        console.log("secondcard");
+        clickedCard = false;
+        cardTwo = this;
+
+        matchCard();
+    };
+};
+
+function matchCard() {
+    if (getCardType(cardOne) === getCardType(cardTwo)) {
+        console.log("match");
+        match();
+        //cardOne.removeEventListener('click', flipCard);
+        //cardTwo.removeEventListener('click', flipCard);
+    } else {
+        console.log("MISmatch");
+        misMatch();
+        //cardOne.classList.remove('flip');
+        //cardTwo.classList.remove('flip');
+    };
+};
+
+function match() {
+    cardOne.removeEventListener('click', flipCard);
+    cardOne.classList.remove('hover');
+    console.log(cardOne);
+    cardTwo.removeEventListener('click', flipCard);
+    //.classList.remove('card:hover');
+    console.log(cardTwo);
+};
+
+function misMatch() {
+    cardOne.classList.remove('flip');
+    cardTwo.classList.remove('flip');
+}
+
+function getCardType(card) {
+   let dataCard = card.getAttribute('data-card');
+   return dataCard;
+};
+/*
+function getCard() {
+    let clickedCard = card.getElementsByClassName('card');
+    console.log(clickedCard);
+};
+
+function startGame() {
+    onOverlay();
+}
+
 //overlay functions from w3schools https://www.w3schools.com/howto/howto_css_overlay.asp 
 function onOverlay() {
   document.getElementById("overlay").style.display = "block";
@@ -7,12 +72,7 @@ function onOverlay() {
 function offOverlay() {
   document.getElementById("overlay").style.display = "none";
 }
+*/
 
-const cards = document.querySelectorAll('.card');
+cardsArr.forEach(card => card.addEventListener('click', flipCard));
 
-function flipCard() {
-    this.classList.toggle('flip');
-    console.log('i was flipped');
-}
-
-cards.forEach(card => card.addEventListener('click', flipCard));
